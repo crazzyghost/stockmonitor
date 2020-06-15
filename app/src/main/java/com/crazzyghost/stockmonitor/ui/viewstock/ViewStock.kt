a129676845b3992b879dc4d129c759f2fdbfdd81
crazzyghost <ssefayeboah1@gmail.com>
2020-06-15 17:47:19 +0000

    plot graph

--- app/src/main/java/com/crazzyghost/stockmonitor/ui/viewstock/ViewStock.kt
+++ app/src/main/java/com/crazzyghost/stockmonitor/ui/viewstock/ViewStock.kt
@@ -1,19 +1,25 @@
 package com.crazzyghost.stockmonitor.ui.viewstock
 
-import android.graphics.drawable.GradientDrawable
+import android.R.attr.entries
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View.GONE
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.crazzyghost.alphavantage.timeseries.response.QuoteResponse
-import com.crazzyghost.alphavantage.timeseries.response.StockUnit
 import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse
 import com.crazzyghost.stockmonitor.R
 import com.crazzyghost.stockmonitor.app.App
 import com.crazzyghost.stockmonitor.util.Constants.EXTRA_STOCK_NAME
 import com.crazzyghost.stockmonitor.util.Constants.EXTRA_STOCK_SYMBOL
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_view_stock.*
 import java.text.DecimalFormat
 import javax.inject.Inject
+
 
 class ViewStock : AppCompatActivity(), ViewStockContract.View {
 
@@ -36,8 +42,20 @@ class ViewStock : AppCompatActivity(), ViewStockContract.View {
     private fun initUi(){
         presenter.fetchDaily(symbol)
         presenter.fetchQuote(symbol)
+
         companyNameTv.text = name
         companySymbolTv.text = symbol
+        graph.setNoDataText("")
+        graph.axisRight.isEnabled = false
+        graph.axisLeft.isEnabled = true
+        graph.axisLeft.axisLineColor = Color.parseColor("#FFFFFFFF")
+        graph.axisLeft.setDrawAxisLine(false)
+        graph.axisLeft.setDrawTopYLabelEntry(true)
+        graph.axisLeft.setDrawLabels(false)
+        graph.xAxis.isEnabled = false
+        graph.legend.isEnabled = false
+        graph.description.isEnabled = false
+        graph.fitScreen()
     }
 
     override fun onResume() {
@@ -51,24 +69,46 @@ class ViewStock : AppCompatActivity(), ViewStockContract.View {
     }
 
     override fun onQuoteResult(response: QuoteResponse) {
-        val fmt = DecimalFormat("#,###.00")
-        previousCloseTv.text = fmt.format(response.previousClose)
-        openTv.text = fmt.format(response.open)
-        highTv.text = fmt.format(response.high)
-        lowTv.text = fmt.format(response.low)
-        volumeTv.text = fmt.format(response.volume)
-        bannerHighTv.text = fmt.format(response.high)
-        percentChangeTv.text =  if (response.changePercent > 0) "+" else "" + fmt.format(response.changePercent) + "%"
+        if(response.errorMessage == null){
+            val fmt = DecimalFormat("#,###0.00")
+            previousCloseTv.text = fmt.format(response.previousClose)
+            openTv.text = fmt.format(response.open)
+            highTv.text = fmt.format(response.high)
+            lowTv.text = fmt.format(response.low)
+            volumeTv.text = fmt.format(response.volume)
+            bannerHighTv.text = fmt.format(response.high)
+            val sign : String = if (response.changePercent > 0) "+" else ""
+            percentChangeTv.text =  sign + fmt.format(response.changePercent) + "%"
 
-        val resource: Int = if (response.changePercent > 0)
-            R.drawable.background_positive_change else R.drawable.background_negative_change
+            val resource: Int = if (response.changePercent > 0)
+                R.drawable.background_positive_change else R.drawable.background_negative_change
 
-        percentChangeTv.setBackgroundResource(resource)
+            percentChangeTv.setBackgroundResource(resource)
+        }else{
+            Toast.makeText(this, response.errorMessage, Toast.LENGTH_LONG).show()
+        }
 
     }
 
     override fun onTimeSeriesResult(response: TimeSeriesResponse) {
         progressBar.visibility = GONE
-        response.stockUnits.forEach(::println)
+        if(response.errorMessage == null){
+
+            val entries = mutableListOf<Entry>()
+            for(i in 0 until response.stockUnits.size){
+                entries.add(Entry(i.toFloat(), response.stockUnits[i].high.toFloat()))
+            }
+            val dataSet = LineDataSet(entries, "")
+            dataSet.lineWidth = 1f
+            dataSet.setDrawCircles(false)
+            dataSet.resetColors()
+            dataSet.color = Color.parseColor("#FFFFFFFF")
+            dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+            val data = LineData(dataSet)
+            graph.data = data
+            graph.invalidate()
+        }else{
+            Toast.makeText(this, response.errorMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
