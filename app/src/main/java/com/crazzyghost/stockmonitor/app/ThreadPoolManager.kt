c82044df117e19c2e353586f011118d965aa94ce
crazzyghost <ssefayeboah1@gmail.com>
2020-11-14 04:04:07 +0000

    remove app executors

--- app/src/main/java/com/crazzyghost/stockmonitor/app/ThreadPoolManager.kt
+++
@@ -1,9 +0,0 @@
-package com.crazzyghost.stockmonitor.app
-
-import java.util.concurrent.Executor
-
-interface ThreadPoolManager {
-    fun main(): Executor
-    fun diskIO(): Executor
-    fun network(): Executor
-}
